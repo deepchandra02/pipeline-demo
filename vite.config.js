b11f5a98bb1854398e41e193a7d95bcfb5f54871
Deep Chandra <thedeepchandra@gmail.com>
2025-03-19 04:59:23 +0300

    updated for flask proxy

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,13 @@ export default defineConfig({
     host: true,
     port: 3000,
     open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   resolve: {
     alias: {
